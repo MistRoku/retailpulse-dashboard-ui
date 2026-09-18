141ea7920fd8762deafd0a918507ecfcf8afecac
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-18 19:25:09 +0200

    Implementing the logic of the Pos controller while adding the use function for the staff controller

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\RetailPulseData;
+
 use Illuminate\Http\Request;
 
 class PosController extends Controller
 {
-    //
+    public function terminal()
+        {
+            return view('pos.terminal', [
+                'products' => RetailPulseData::products(),
+                'categories' => RetailPulseData::categories(),
+                'heldOrders' => RetailPulseData::heldOrders(),
+                'taxRate' => 7.5,
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                    ['label' => 'Point of Sale', 'url' => route('pos.terminal')],
+                ],
+            ]);
+        }
 }
--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Support\RetailPulseData;
+
 class StaffController extends Controller
 {
     public function index()
