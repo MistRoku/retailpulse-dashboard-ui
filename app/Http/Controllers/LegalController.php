b43c38959d31460af8df43f47e12ff549e2e8dfe
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-18 19:35:26 +0200

    implemented the logic of the legal and settings controller

--- app/Http/Controllers/LegalController.php
+++ app/Http/Controllers/LegalController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class LegalController extends Controller
 {
-    //
+    public function terms()
+        {
+            return view('legal.terms', [
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                    ['label' => 'Terms of Service', 'url' => route('legal.terms')],
+                ],
+            ]);
+        }
+
+        public function privacy()
+        {
+            return view('legal.privacy', [
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                    ['label' => 'Privacy Policy', 'url' => route('legal.privacy')],
+                ],
+            ]);
+        }
 }
--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function index()
+       {
+           return view('settings.index', [
+               'breadcrumbs' => [
+                   ['label' => 'Dashboard', 'url' => route('dashboard')],
+                   ['label' => 'Settings', 'url' => route('settings.index')],
+               ],
+           ]);
+       }
 }
