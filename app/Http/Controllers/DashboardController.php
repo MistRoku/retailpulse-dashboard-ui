37f0c688d0dbdddd631252307d342a7ece994d68
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-18 19:17:18 +0200

    Implementing the logic of the Dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\RetailPulseData;
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+        {
+            return view('dashboard.index', [
+                'kpis' => RetailPulseData::kpis(),
+                'revenueSeries' => RetailPulseData::revenueSeries(),
+                'topProducts' => RetailPulseData::topProducts(),
+                'transactions' => RetailPulseData::recentTransactions(),
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                ],
+            ]);
+        }
 }
