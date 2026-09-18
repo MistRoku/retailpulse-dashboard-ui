5de9f4b4b63fcc3a720bc3fcad9fd66985f38537
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-18 19:20:24 +0200

    Implementing the logic of the Staff controller

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
-    //
+    public function index()
+        {
+            return view('products.index', [
+                'products' => RetailPulseData::products(),
+                'categories' => RetailPulseData::categories(),
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                    ['label' => 'Products', 'url' => route('products.index')],
+                ],
+            ]);
+        }
 }
