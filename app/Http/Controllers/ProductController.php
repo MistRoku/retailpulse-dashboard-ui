73b554753d1bafad11dc18cb75bf9200241f3088
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-18 19:18:14 +0200

    Implementing the logic of the Product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\RetailPulseData;
+
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+        {
+            return view('products.index', [
+                'products' => RetailPulseData::products(),
+                'categories' => RetailPulseData::categories(),
+                'breadcrumbs' => [
+                    ['label' => 'Dashboard', 'url' => route('dashboard')],
+                    ['label' => 'Products', 'url' => route('products.index')],
+                ],
+            ]);
+        }
 }
